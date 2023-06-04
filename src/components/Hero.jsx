2472d2bd07d0e4f0adb6e0005bcc9b26bd7005c3
Tushar-Mishra999 <75738710+Tushar-Mishra999@users.noreply.github.com>
2023-06-04 14:02:38 +0530

    intro section fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,29 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+// import { fadeIn, textVariant } from "../utils/motion";
+const textVariant =()=> {
+  return {
+  hidden: { opacity: 0, y: 40 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.4 } }
+}
+};
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    
+    <section className={`relative w-full h-screen mx-auto `}>
+      <motion.div variants={textVariant()} initial="hidden" animate="visible">
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto mt-20 ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
-
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+          <h1 className={`${styles.heroHeadText} text-white mt-24`}>
+            Hi, I'm <span className='text-[#915EFF]'>Tushar</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
@@ -25,7 +32,8 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+        </motion.div>
+      {/* <ComputersCanvas /> */}
 
       {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -71,11 +71,11 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        <p className={`${styles.sectionSubText} mx-auto flex justify-center`}>My work</p>
+        <h2 className={`${styles.sectionHeadText} flex justify-center`}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      {/* <div className='w-full flex'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
@@ -86,7 +86,7 @@ const Works = () => {
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </motion.p>
-      </div>
+      </div> */}
 
       <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
