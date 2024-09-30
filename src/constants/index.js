295d527a7693d67b6bbb8ad6384490e4cee0dd76
Tushar-Mishra16 <tushar.mishra@mindler.com>
2024-09-30 21:36:10 +0530

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,11 +39,10 @@ const experiences = [
     company_name: "Mindler",
     icon: irdeto,
     iconBg: "#ffffff",
-    date: "Feb 2024 - Present",
+    date: "Feb 2024 - Sept 2024",
     points: [
      "Created the frontend, backend and database infrastructure for the admin feature and the frontend of the landing page of UNIVERZE project , utilizing React.js, Node.js, MySQL and Typescript.",
-    "Contributed to the full-stack development for two career counselling platforms, utilizing Node.js for API development alongside AngularJS for frontend implementation.",
-      
+    "Contributed to the full-stack development for three career counselling platforms, utilizing Node.js for API development alongside AngularJS for frontend implementation.",
     ],
   },
   {
